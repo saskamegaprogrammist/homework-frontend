9cb53c2c62c8f54b6aed7619817287fcba44f629
spiridonovaalex@gmail.com <spiridonovaalex@gmail.com>
2019-09-02 10:30:42 +0300

    [UPD]plainify func

--- source/plainify.js
+++ source/plainify.js
@@ -6,21 +6,11 @@ function plainify(object){
         if (typeof object[key] == 'object') {
             let newObject = plainify(object[key]);
             for (let field in newObject){
-                //console.log(newObject);
                 oldObject[`${key}.${field}`]=newObject[field];
             }
         } else {
-            //key=String(key);
             oldObject[`${key}`] = object[key];
         }
     }
     return oldObject;
-}/*
-const nested1 = {
-    deep: {
-        foo: 'bar',
-        baz: 42
-    }
-};
-let plain = plainify({foo: 'bar', baz: 42});
-console.log(plain);*/
+}
